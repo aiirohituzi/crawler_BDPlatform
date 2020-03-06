6448efa53305989edfed97dbc380696d1ac48c18
aiirohituzi <sinn5656@gmail.com>
2020-03-06 10:03:48 +0900

    타겟 지역의 빠른 전환을 위해 url, 지역명칭 등을 전부 변수화

--- capture.py
+++ capture.py
@@ -7,6 +7,12 @@ from selenium.webdriver.common.by import By
 import time
 import config
 
+# 지역 변경 시 이 변수만 url 맞춰서 수정
+# id, pw 값은 별개로 config.py 들어가서 일일히 직접 수정해주어야 함
+# 논산, 광명, 금산은 메뉴항목이 다르므로 우선순위 나중에
+# local 명칭으로 폴더 하나 만들어 줘야 함
+local = 'hwasung'
+
 
 def crop_image(img):
     img_width, img_height = img.size
@@ -23,13 +29,13 @@ driver = webdriver.Chrome('./chromedriver/chromedriver',chrome_options=options)
 
 driver.set_window_size(1920, 1080) # width , height
 
-driver.get(config.target_login_url)
+driver.get(f'{config.target_login_url}{local}/')
 driver.maximize_window()
 driver.find_element_by_xpath('/html/body/div/a[1]/img').click()
 driver.find_element_by_name('userId').send_keys(config.User_id)
 driver.find_element_by_name('password').send_keys(config.User_pw)
 driver.find_element_by_xpath('//*[@id="loginBtn"]').click()
-print("로그인 시도 중")
+print(f"{config.target_login_url}{local}에 로그인 시도 중")
 # driver.implicitly_wait(5)
 
 wait = WebDriverWait(driver, 5)
@@ -37,11 +43,13 @@ wait.until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "#iframe-content"))
 )
 
-for params in config.nonsan_menu_param:
-    driver.get(f'{config.target_menu_url}{params}')
+# for params in config.nonsan_menu_param:
+# for params in config.temp_param:
+for params in config.gugun_menu_param:
+    driver.get(f'{config.target_login_url}{local}{config.target_menu_url}{params}')
     print(f'[param:{params}] 태블로 화면 로딩 중')
     # driver.implicitly_wait(10)
-    time.sleep(10)
+    time.sleep(25)
     # wait = WebDriverWait(driver, 5)
     # wait.until(
     #     EC.presence_of_element_located((By.CSS_SELECTOR, "#centeringContainer"))
@@ -51,7 +59,7 @@ for params in config.nonsan_menu_param:
     img_binary = driver.get_screenshot_as_png()
     img = Image.open(BytesIO(img_binary))
     img = crop_image(img)
-    img.save(f'./nonsan/{params}.png')
+    img.save(f'./{local}/{params}.png')
 
     # driver.save_screenshot("screenshot.png")
 
